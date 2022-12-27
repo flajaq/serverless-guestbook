3ae9ed0a917a1ad6e7aac68f0b1b3264fc662fd7
flajaq <93446950+flajaq@users.noreply.github.com>
2022-12-27 16:33:53 +0100

    Changed urls and endpoints

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/5eedaae5-666b-408c-959a-e8d56cab7aa1/guestbook/';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${/read-guestbook-entries-sequence.json}/entries`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${/save-guestbook-entry-sequence.json}/entries`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
